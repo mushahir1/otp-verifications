234e64bfb08346714731a061c3eab4302492a033
mushahir1 <138847795+mushahir1@users.noreply.github.com>
2024-01-31 14:50:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
     DB_CON: 'mongodb://localhost:27017/latestdb',
     BASE_URL: 'http://localhost:4000',
     EMAIL_SERVICE: 'gmail',
-    EMAIL_USER: 'mushahirhusain@gmail.com',
-    EMAIL_PASSWORD: 'riwf tngt ylag ymwu',
-    EMAIL_FROM: 'mushahirhusain@gmail.com',
+    EMAIL_USER: '',
+    EMAIL_PASSWORD: '',
+    EMAIL_FROM: '',
     JWT_SECRET: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGY1NjkxYjVhMmU5YWYwNDA0YWViNzAiLCJpYXQiOjE2OTM4MDYxMzUsImV4cCI6MTY5MzgwOTczNX0.c1BrOy3zYHuf7fO_sRl1NEaknCFqcIKmEpR2kCFZVXk'
 }
